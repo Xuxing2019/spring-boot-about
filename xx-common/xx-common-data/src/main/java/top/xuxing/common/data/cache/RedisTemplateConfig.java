69862e62a5d24f2d1e59e381158edbf9f4495648
Xuxing <104206034@qq.com>
2021-02-12 22:50:32 +0800

    update

--- xx-common/xx-common-data/src/main/java/top/xuxing/common/data/cache/RedisTemplateConfig.java
+++ xx-common/xx-common-data/src/main/java/top/xuxing/common/data/cache/RedisTemplateConfig.java
@@ -2,6 +2,7 @@ package top.xuxing.common.data.cache;
 
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,9 +11,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @author xhb
  * @since 2021/2/11
  */
+@Configuration
 public class RedisTemplateConfig {
 
-    @Bean("xxRedisTemplate")
+    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
