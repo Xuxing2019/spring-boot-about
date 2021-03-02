ab3d900f0aca993484da7e906bb58e7d7b97e9f9
Xuxing <104206034@qq.com>
2021-03-02 14:21:10 +0800

    gateway learn

--- xx-upms/xx-upms-biz/src/main/java/top/xuxing/upms/controller/RedisQueueTestApi.java
+++ xx-upms/xx-upms-biz/src/main/java/top/xuxing/upms/controller/RedisQueueTestApi.java
@@ -3,6 +3,7 @@ package top.xuxing.upms.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.xuxing.common.data.redis.pc.config.RedisQueueConfiguration;
 import top.xuxing.common.data.redis.pc.msg.MsgConsumer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 /**
  * @author xhb
@@ -32,5 +36,25 @@ public class RedisQueueTestApi {
             redisTemplate.opsForList().leftPush(consumerTopic, "red shit " + i);
         }
         return ResponseEntity.ok("success");
+    }
+
+    @GetMapping("gateway")
+    public ResponseEntity<String> gatewayTest(HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            System.out.print(headerNames.nextElement() + " ");
+        }
+        System.out.println("");
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()){
+            System.out.print(parameterNames.nextElement() + " ");
+        }
+        System.out.println("");
+        return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> defaultPath(){
+        return ResponseEntity.ok("ok");
     }
 }
--- xx-upms/xx-upms-biz/src/test/java/BaseTest.java
+++ xx-upms/xx-upms-biz/src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.ObjectUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,12 +9,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 import top.xuxing.common.data.cache.Util;
 import top.xuxing.upms.AdminApplication;
 
-import java.util.Collections;
-import java.util.HashSet;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author xhb
@@ -45,6 +51,7 @@ public class BaseTest {
      * shutdown 拒接接收新任务，等待在线任务结束再关闭线程池(任务不结束无法关闭线程池)
      * shutdownNow 拒接接收新任务，将线程池设置stop状态，在特定时机结束任务
      * isTerminated 所有任务结束 返回true
+     *
      * @throws InterruptedException
      */
     @Test
@@ -57,21 +64,61 @@ public class BaseTest {
                 new LinkedBlockingQueue<Runnable>(),
                 Executors.defaultThreadFactory(),
                 new ThreadPoolExecutor.AbortPolicy());
-        threadPoolExecutor.submit(()->{
-           while (BaseTest.run){
-               System.out.println("run..." + System.currentTimeMillis());
-           }
+        threadPoolExecutor.submit(() -> {
+            while (BaseTest.run) {
+                System.out.println("run..." + System.currentTimeMillis());
+            }
         });
         TimeUnit.SECONDS.sleep(3);
         BaseTest.run = false;
         threadPoolExecutor.shutdown();
         boolean flag = threadPoolExecutor.awaitTermination(10, TimeUnit.SECONDS);
         System.out.println("flag " + flag);
-        if (threadPoolExecutor.isTerminated()){
+        if (threadPoolExecutor.isTerminated()) {
             System.out.println("pool is down");
-        }else{
+        } else {
             System.out.println("pool not down");
         }
+    }
+
+    @Test
+    public void timeUtil() {
+        LocalDate now = LocalDate.now();
+        LocalDateTime localDateTime = now.atStartOfDay();
+        System.out.println("localDateTime = " + localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        ArrayList<String> times = new ArrayList<>();
+        for (int i = 0; i <= 24; i++) {
+            LocalDateTime currentHour = localDateTime.plusHours(i);
+            times.add(currentHour.format(DateTimeFormatter.ofPattern("yyyyMMddHH")));
+        }
+        System.out.println("times = " + times);
+
+        for (int i = 1; i <= 24; i++) {
+            String currentHour = times.get(i);
+            String previousHour = times.get(i - 1);
+            System.out.println( previousHour+ " ~ " + currentHour);
+        }
+
+        BigDecimal a = new BigDecimal(3);
+        BigDecimal b = new BigDecimal(11);
+        BigDecimal divide = a.divide(b, 4, BigDecimal.ROUND_HALF_UP);
+        System.out.println(divide.doubleValue());
+        NumberFormat numberFormat = NumberFormat.getPercentInstance();
+        numberFormat.setMaximumFractionDigits(4);
+        System.out.println(numberFormat.format(divide.doubleValue()));
+
+        String timeStr = "2021022423";
+        LocalDateTime parse = LocalDateTime.parse(timeStr, DateTimeFormatter.ofPattern("yyyyMMddHH"));
+        System.out.println("parse = " + parse.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        String previousHour = "2021022816";
+        LocalDateTime betweenStart = LocalDateTime.parse(previousHour, DateTimeFormatter.ofPattern("yyyyMMddHH"));
+        String betweenStartStr = betweenStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        LocalDateTime betweenEnd = betweenStart.plusHours(1);
+        String betweenEndStr = betweenEnd.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("betweenStartStr = " + betweenStartStr);
+        System.out.println("betweenEndStr = " + betweenEndStr);
     }
 
 }
